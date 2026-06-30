e1ac6d1ae47c71264505db545314215b28edafa5
yagnik-s <yagnik.s@mykaleidoscope.com>
2026-06-30 07:12:41 +0000

    Build OctoFit backend logic and data tiers

--- octofit-tracker/backend/dist/index.js
+++ octofit-tracker/backend/dist/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const database_1 = require("./database");
+const database_1 = require("./config/database");
 const activities_1 = __importDefault(require("./routes/activities"));
 const leaderboard_1 = __importDefault(require("./routes/leaderboard"));
 const teams_1 = __importDefault(require("./routes/teams"));
--- octofit-tracker/backend/src/index.ts
+++ octofit-tracker/backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { connectDatabase, MONGODB_URI } from './database';
+import { connectDatabase, MONGODB_URI } from './config/database';
 import activitiesRouter from './routes/activities';
 import leaderboardRouter from './routes/leaderboard';
 import teamsRouter from './routes/teams';
